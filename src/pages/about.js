b1e3f417afd9275b0de39625e02694a8cea32924
Myles Enriquez <enri.myles@gmail.com>
2019-04-26 15:52:40 -0700

    Create about page.

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout title="Roll">
+    <h2>About me</h2>
+    <p>My name is Myles.</p>
+  </Layout>
+)
+
+export default About
